a4c83a95a50a8e663698ef2ad3b1a0bcc6a6ac45
victhor souza <victhor.souza@dellead.com>
2022-04-19 16:26:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PaymentsComponent } from './pages/payments/payments.component';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, LoginComponent, PaymentsComponent,
    ],
   imports: [
     BrowserModule
