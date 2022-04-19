a1a6e851097080aa8c2fe3d60abb3fee988c6a03
victhor souza <victhor.souza@dellead.com>
2022-04-19 17:10:38 -0300

    feat: Adiciona módulo de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { PaymentsComponent } from './pages/payments/payments.component';
 @NgModule({
@@ -9,7 +11,9 @@ import { PaymentsComponent } from './pages/payments/payments.component';
     AppComponent, LoginComponent, PaymentsComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
